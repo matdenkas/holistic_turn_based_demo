e3527682703037aa1b9449e0783b2be3e18da329
AlcatrazEscapee <alex@molleroneill.com>
2024-06-08 16:40:24 -0400

    Add a basic impl for the log panel

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,6 @@
 import type { Application, ColorSource, Container, Graphics } from "pixi.js";
 import { Colors, Constants } from "./constants";
+import { LogPanel } from "./logPanel";
 
 
 function backgroundRect(color: ColorSource, w: number, h: number): Graphics {
@@ -17,7 +18,7 @@ export class Game {
     readonly infoPanel: Container;
     readonly actionPanel: Container;
     readonly boardPanel: Container;
-    readonly logPanel: Container;
+    readonly logPanel: LogPanel;
 
     constructor(app: Application) {
         this.app = app;
@@ -27,23 +28,20 @@ export class Game {
         this.infoPanel = new PIXI.Container();
         this.actionPanel = new PIXI.Container();
         this.boardPanel = new PIXI.Container();
-        this.logPanel = new PIXI.Container();
+        this.logPanel = new LogPanel(this.stage);
 
         this.infoPanel.position.set(Constants.INFO_X, Constants.INFO_Y);
         this.actionPanel.position.set(Constants.ACTION_X, Constants.ACTION_Y);
         this.boardPanel.position.set(Constants.BOARD_X, Constants.BOARD_Y);
-        this.logPanel.position.set(Constants.LOG_X, Constants.LOG_Y);
 
         this.infoPanel.addChild(backgroundRect(Colors.LIGHT_CYAN, Constants.INFO_W, Constants.INFO_H));
         this.actionPanel.addChild(backgroundRect(Colors.LIGHT_RED, Constants.ACTION_W, Constants.ACTION_H));
         this.boardPanel.addChild(backgroundRect(Colors.LIGHT_BLUE, Constants.BOARD_W, Constants.BOARD_H));
-        this.logPanel.addChild(backgroundRect(Colors.LIGHT_PINK, Constants.LOG_W, Constants.LOG_H));
 
         this.stage.addChild(
             this.infoPanel,
             this.actionPanel,
             this.boardPanel,
-            this.logPanel
         );
 
         this.stage.on('pointertap', event => this.onPointerTap(event));
---
+++ src/logPanel.ts
@@ -0,0 +1,80 @@
+import { Container, Text } from "pixi.js";
+import { Colors, Constants } from "./constants";
+
+
+const PADDING: number = 10;
+const PADDING_HALF: number = 5;
+
+
+/**
+ * The panel to the right hand side of the screen, which records actions taken. 
+ */
+export class LogPanel {
+
+    private readonly messages: ({ text: string, obj: Text })[];
+    private messageHeight: number; // Total height of all messages (including padding)
+
+    private readonly root: Container;
+    private readonly width: number;
+    private readonly height: number;
+
+    constructor(parent: Container) {
+        this.messages = [];
+        this.root = new PIXI.Container();
+        this.root.position.set(0.8 * Constants.WIDTH, 0);
+        this.width = 0.2 * Constants.WIDTH;
+        this.height = Constants.HEIGHT;
+        this.messageHeight = 0;
+
+        const rect = new PIXI.Graphics()
+            .beginFill(Colors.LIGHT_PINK)
+            .drawRect(0, 0, this.width, this.height)
+            .endFill();
+
+        this.root.addChild(rect);
+        parent.addChild(this.root);
+    }
+
+    /**
+     * Post a message to the log panel. Displays in order of posting, bottom to top, oldest messages are removed
+     * @param message The message information, with optional color
+     */
+    public post(text: string): void {
+        const obj = new PIXI.Text(text, {
+            fontFamily: 'Arial',
+            fontWeight: 'normal',
+            fontSize: 28,
+            fill: 'black',
+            wordWrap: true,
+            wordWrapWidth: this.width - PADDING_HALF
+        });
+        
+        this.messages.push({ text, obj });
+        this.messageHeight += obj.height + PADDING;
+        this.root.addChild(obj);
+
+        // Ensure that the total message height is smaller than the space we have
+        while (this.messageHeight > this.height) {
+            const msg = this.messages.shift()!;
+            this.messageHeight -= msg.obj.height + PADDING;
+            msg.obj.removeFromParent();
+        }
+
+        // Then position all messages from top to bottom
+        let y = PADDING_HALF;
+        for (const msg of this.messages) {
+            msg.obj.position.set(PADDING_HALF, y);
+            y += msg.obj.height + PADDING;
+        }
+    }
+
+    /**
+     * Clear all displayed log messages
+     */
+    public clear(): void {
+        for (const msg of this.messages) {
+            msg.obj.removeFromParent();
+        }
+        (this.messages as any) = [];
+    }
+}
